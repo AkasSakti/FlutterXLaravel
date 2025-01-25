83e1d36f6f8f8a7ed921335b1079c0d8527c93fc
Akas Bagus Setiawan <146935384+AkasSakti@users.noreply.github.com>
2025-01-25 20:22:01 +0700

    RuanganController.php

--- Laravel/App/Http/Controller/RuanganController.php
+++ Laravel/App/Http/Controller/RuanganController.php
@@ -1,1 +1,34 @@
+   use App\Models\Ruangan;
+   use Illuminate\Http\Request;
 
+   class RuanganController extends Controller
+   {
+       public function index()
+       {
+           return Ruangan::all();
+       }
+
+       public function store(Request $request)
+       {
+           $ruangan = Ruangan::create($request->all());
+           return response()->json($ruangan, 201);
+       }
+
+       public function show($id)
+       {
+           return Ruangan::find($id);
+       }
+
+       public function update(Request $request, $id)
+       {
+           $ruangan = Ruangan::findOrFail($id);
+           $ruangan->update($request->all());
+           return response()->json($ruangan, 200);
+       }
+
+       public function destroy($id)
+       {
+           Ruangan::destroy($id);
+           return response()->json(null, 204);
+       }
+   }
